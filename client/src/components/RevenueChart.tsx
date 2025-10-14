56302b7a4ae4a8ce773ae8bb911a937f45b500f2
jakefrompopeyes <jakefrompopeyes@users.noreply.github.com>
2025-10-14 14:38:12 -0500

    RevenueChart: set y-axis min=0 to prevent negative values

--- client/src/components/RevenueChart.tsx
+++ client/src/components/RevenueChart.tsx
@@ -118,6 +118,7 @@ export default function RevenueChart({ data, className = '' }: RevenueChartProps
           display: true,
           color: 'rgba(0, 0, 0, 0.05)'
         },
+        min: 0,
         ticks: {
           font: {
             size: 11
